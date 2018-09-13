9c970b988f0d16d13bb586fbbf0ed05d84d30694
Asghar Razavi <asghar.razavi@gmail.com>
2018-09-13 14:05:51 -0400

    load-with-stride.tcl

--- load-with-stride.tcl
+++ load-with-stride.tcl
@@ -2,6 +2,7 @@
 # add a molecule
 mol new  NKA_4hqj.psf type psf first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
 
+# add first 10
 for {set i 1} {$i < 9} {incr i} {
     mol addfile NKA_4hqj_0000$i.coor.dcd type dcd first 0 last -1 step 10 filebonds 1 autobonds 1 waitfor all
 }
