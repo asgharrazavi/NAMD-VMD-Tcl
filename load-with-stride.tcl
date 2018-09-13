9a4b30512bb5de0b5880e09821eed9d87fddddca
Asghar Razavi <asghar.razavi@gmail.com>
2018-09-13 14:02:07 -0400

    load-with-stride.tcl

---
+++ load-with-stride.tcl
@@ -0,0 +1,8 @@
+mol new  NKA_4hqj.psf type psf first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+
+for {set i 1} {$i < 9} {incr i} {
+    mol addfile NKA_4hqj_0000$i.coor.dcd type dcd first 0 last -1 step 10 filebonds 1 autobonds 1 waitfor all
+}
+for {set i 10} {$i < 60} {incr i} {
+    mol addfile NKA_4hqj_000$i.coor.dcd type dcd first 0 last -1 step 10 filebonds 1 autobonds 1 waitfor all
+}
