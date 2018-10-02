787e61f5c5444d703ec81c363a5ff661f3cc616a
Asghar Razavi <asghar.razavi@gmail.com>
2018-10-02 13:30:27 -0400

    volmap_pme.tcl

--- utils/volmap_pme.tcl
+++ utils/volmap_pme.tcl
@@ -9,6 +9,7 @@ volmap density [atomselect 0 "segname PROA and resid 926"] -res 0.5 -weight mass
 volmap density [atomselect 0 "segname PROA and resid 327"] -res 0.5 -weight mass -allframes -combine avg -mol top -checkpoint 0 -minmax {{-60 -60 -120} {60 60 120}} -o e327_box.dx
 volmap density [atomselect 0 "segname PROA and resid 779"] -res 0.5 -weight mass -allframes -combine avg -mol top -checkpoint 0 -minmax {{-60 -60 -120} {60 60 120}} -o e779_box.dx
 
+# calculate electrostatics
 pmepot -sel [atomselect 0 "not segname PROA and resid 804"] -ewaldfactor 5.0 -grid 0.5  -frames all -updatesel yes -dxfile pme_d804_box.dx -cell  {{0 0 0} { 120 0 0} { 0 120 0} { 0 0 240 }}
 pmepot -sel [atomselect 0 "not segname PROA and resid 808"] -ewaldfactor 5.0 -grid 0.5  -frames all -updatesel yes -dxfile pme_d808_box.dx -cell  {{0 0 0} { 120 0 0} { 0 120 0} { 0 0 240 }}
 pmepot -sel [atomselect 0 "not segname PROA and resid 926"] -ewaldfactor 5.0 -grid 0.5  -frames all -updatesel yes -dxfile pme_d926_box.dx -cell  {{0 0 0} { 120 0 0} { 0 120 0} { 0 0 240 }}
