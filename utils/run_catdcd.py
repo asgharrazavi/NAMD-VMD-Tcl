b5dbed91ee6b6ae105253b79d7df380f4d9180e1
Asghar Razavi <asghar.razavi@gmail.com>
2018-10-04 13:03:06 -0400

    run_catdcd.py

---
+++ utils/run_catdcd.py
@@ -0,0 +1,10 @@
+import os,sys
+
+text = ''
+for i in range(25,32):
+    text += '../3kdp_eq_nod804_000%d.coor.dcd  ' %i
+for i in range(10,25):
+    text += '../3kdp_eq_000%d.coor.dcd  ' %i
+
+os.system('./catdcd -i ind.ind -o all-protein-pot.dcd %s' %text)
+
