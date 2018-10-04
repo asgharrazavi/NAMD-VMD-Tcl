91321967a152329c6a6c72d5e5ad699b6cc2842d
Asghar Razavi <asghar.razavi@gmail.com>
2018-10-04 13:03:35 -0400

    run_catdcd.py

--- utils/run_catdcd.py
+++ utils/run_catdcd.py
@@ -1,10 +1,13 @@
 import os,sys
 
+# generate input text file
 text = ''
 for i in range(25,32):
-    text += '../3kdp_eq_nod804_000%d.coor.dcd  ' %i
+    text += '3kdp_eq_nod804_000%d.coor.dcd  ' %i
 for i in range(10,25):
-    text += '../3kdp_eq_000%d.coor.dcd  ' %i
+    text += '3kdp_eq_000%d.coor.dcd  ' %i
+
+
 
 os.system('./catdcd -i ind.ind -o all-protein-pot.dcd %s' %text)
 
