d8116d9e38552d7bddc822d837c656f6a933a4ff
Asghar Razavi <asghar.razavi@gmail.com>
2019-04-30 09:06:58 -0400

    tk_mv2.tcl

--- tk_mv2.tcl
+++ tk_mv2.tcl
@@ -1,5 +1,6 @@
 # script for calculating velocities (MV**2) of atoms based on *.vel file
-# loa
+# load *.vel in frame 1 as moladdfile *.vel type namdbin
+
 set aa [atomselect top "protein and resid 1 to 620 and name CA" frame 1]
 set v2 [vecadd [vecmul [$aa get x] [$aa get x] ] [vecmul [$aa get y] [$aa get y]] [vecmul [$aa get z] [$aa get z]] ]
 set mv2 [vecmul [$aa get mass] [exp {$v2}]]
